e2b0f37c2d7d083eba039933f64e81524522137b
Pradeep Saran <pradeepmadasar@gmail.com>
2024-09-02 01:49:24 +0530

    search bar

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -10,6 +10,20 @@ function HomePage() {
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi placeat exercitationem reiciendis dolorum adipisci! Corrupti impedit doloremque, rerum adipisci similique placeat! Esse nesciunt rerum, delectus odio sapiente quo dicta explicabo.
             </p>
             <SearchBar />
+            <div className="boxes">
+                <div className="box">
+                    <h1>16+</h1>
+                    <h2>Years of Experience</h2>
+                </div>
+                <div className="box">
+                    <h1>200</h1>
+                    <h2>Award Gained</h2>
+                </div>
+                <div className="box">
+                    <h1>1200+</h1>
+                    <h2>Property Ready</h2>
+                </div>
+            </div>
         </div>
       </div>
       <div className="imgContainer">
